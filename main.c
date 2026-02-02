cb5624ebe694645efb64d0e624b56d415fb8b237
suyash345 <suyashkhanna345@gmail.com>
2026-02-02 21:17:25 +1030

    added mpc

--- main.c
+++ main.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "mpc.h"
 
-/* Declare a buffer for user input of size 2048 static makes it local to this file.*/
-static char input[2048];
+/* If WINDOWS */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* ELSE */
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
-
-  /* Print Version and Exit Information */
-  puts("Lispy Version 0.0.0.0.1");
+  puts("Lispy Version Bob");
   puts("Press Ctrl+c to Exit\n");
 
-  /* In a never ending loop */
-  while (1) {
-
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
-
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
-
-    /* Echo input back to user */
-    printf("No you're a %s", input);
+  while(1) {
+    char* input = readline("lispy> ");
+    add_history(input);
+    printf("No you're a %s\n",input);
+    free(input);
   }
-
   return 0;
 }

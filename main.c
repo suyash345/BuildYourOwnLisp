9bc098cd43809c7209d2ad046154d7bab3b686c4
suyash345 <suyashkhanna345@gmail.com>
2026-02-02 21:46:49 +1030

    chapter 6

--- main.c
+++ main.c
@@ -27,14 +27,43 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+  mpc_parser_t* num = mpc_new("num");
+  mpc_parser_t* op = mpc_new("op");
+  mpc_parser_t* ex = mpc_new("ex");
+  mpc_parser_t* lispy = mpc_new("lispy");
+
+mpca_lang(MPCA_LANG_DEFAULT,
+  "                                       \
+    num   : /-?[0-9]+/ ;                  \
+    op : '+' | '-' | '*' | '/' ;          \
+    ex     : <num> | '(' <op> <ex>+ ')' ; \
+    lispy    : /^/ <op> <ex>+ /$/ ;       \
+  ",
+  num,op, ex, lispy);
+
   puts("Lispy Version Bob");
   puts("Press Ctrl+c to Exit\n");
-
+  
   while(1) {
     char* input = readline("lispy> ");
     add_history(input);
-    printf("No you're a %s\n",input);
+    mpc_result_t result;
+    if(mpc_parse("<stdin>",input,lispy,&result)) {
+      // print the output that is read in.
+      mpc_ast_print(result.output);
+      mpc_ast_delete(result.output);
+    } 
+    else {
+      // print the error otherwise
+      mpc_err_print(result.error);
+      mpc_err_delete(result.error);
+
+    }
+
+
     free(input);
   }
+  mpc_cleanup(4,num,op,ex,lispy);
+
   return 0;
 }
